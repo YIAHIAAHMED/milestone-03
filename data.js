2629cc455697d1b2ad5a2b4288cd3d56bbca351b
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-01-22 20:34:50 +0600

    js

---
+++ data.js
@@ -0,0 +1,13 @@
+var name ='Yiahia Ahmed'
+console.log(name);
+var address= "Noya Gram, Horipur Bazar, Jaintapur, Sylhet";
+var country = 'Bangladesh';
+// boolean
+var isRich =true;
+console.log(isRich)
+var passed = false;
+console.log(passed);
+console.log(typeof passed)
+console.log(typeof name)
+var price = 555;
+console.log(typeof price)
