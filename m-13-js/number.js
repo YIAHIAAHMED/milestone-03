c436ef417b4f76f3f29ae4f68a6f800f41adb593
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-01-23 19:05:23 +0600

    2

---
+++ m-13-js/number.js
@@ -0,0 +1,56 @@
+// var orangePrice =20;
+// var chocolate =0.5;
+// var applePrice ='25';
+// console.log(orangePrice);
+// console.log(applePrice);
+// console.log(typeof orangePrice);
+// console.log(typeof applePrice);
+// console.log(orangePrice + applePrice);
+// var first = 0.1;
+// var second =0.2;
+// var total = first + second;
+// console.log(total.toFixed(1));
+// const num =-50/0;
+// console.log(num);
+// var onionPrice = 20;
+// var eggPrice = 10;
+// var totalPrice =(onionPrice + eggPrice);
+// console.log(totalPrice);
+// var price =35;
+// // price =price+10;
+// price +=10
+// console.log(price);
+// var fridge =35000;
+// fridge -= 1000;
+// console.log(fridge);
+// var onionPrice =30;
+// onionPrice*=3;
+// console.log(onionPrice);
+// var first ='Mobarok';
+// var second ='Toborok';
+// var fullName =first + second:
+// console.log(fullName);
+// var haveTaka = 1000;
+// var orangePrice =150;
+// var applePrice =150;
+// var totalHave =haveTaka-(orangePrice+ applePrice);
+// console.log(totalHave);
+// var mathematics=75.25;
+// var biology = 65;
+// var chemistry = 80;
+// var physics =35.45;
+// var bangla = 99.50;
+// var results = mathematics+biology+chemistry+physics+bangla;
+// var average=results/5;
+// console.log(average.toFixed(2));
+var num =119;
+num %=5;
+console.log(num);
+const a1 =isNaN('11');
+const a2 =isNaN(2-10);
+console.log(a1);
+console.log(a2);
+ 
+
+
+
