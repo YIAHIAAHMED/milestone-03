c0b214ad3402f1b797818c020ea76b26c603d2e1
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-01-27 07:05:12 +0600

    loop

---
+++ m-15/15-3-while-loop.js
@@ -0,0 +1,40 @@
+// let num =1;
+// while(num<100){
+//     console.log(num);
+//     num++;
+// }
+
+// let num =1;
+// let sum =0;
+// while(num <= 12){
+//     console.log(num);
+//     sum= sum + num;
+//     console.log('sum:',sum);
+//     num++;
+// }
+
+
+// let  num =1;
+// while (num<=10) {
+    // console.log(num);
+//     if (num%2===0) {
+//         console.log('even number', num);
+//     }
+// num ++;
+// }
+
+// for (let i=0; i<=15; i++){
+//     console.log(i);
+//     if (i>=6) {
+//         break;
+//     }
+// }
+
+let n= 54;
+while(n>25){
+    console.log(n);
+    if (n<50) {
+        break;
+    }
+    n--;
+}
